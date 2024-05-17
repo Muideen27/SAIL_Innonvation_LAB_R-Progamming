fd86764273373699ae5711446ca277ce1fc54494
Muideen27 <ilorimuideen0000@gmail.com>
2024-05-17 10:44:27 +0100

    manipulating nrow and ncol

--- R_data_structure/2_matrix.R
+++ R_data_structure/2_matrix.R
@@ -17,4 +17,18 @@
 # We will create a 4 X 4 order matrix with values from 1 to 16 (column-wise order).
 
 matrix_1 <- matrix(c(1:16), ncol = 4)
+# c(1:16) is a vactor of numbers from 1 to 16
+# ncol = number of column 
 matrix_1
+
+muideen <- matrix(c(1:20), ncol = 4, nrow = 5)
+muideen
+
+# non-sequential vector
+
+yemi = matrix(c(33, 23, 87, 98, 98, 70), ncol = 3, nrow = 2)
+yemi
+
+
+rownames <- c("row1", "row2", "row3", "row4")
+colnames <- c("col1", "col2", "col3")
