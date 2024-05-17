5e97e5037a7913fc758800201230a39d4d6185fe
Muideen27 <ilorimuideen0000@gmail.com>
2024-05-17 10:08:42 +0100

    Learning List DS in R

---
+++ R_data_structure/1_list.R
@@ -0,0 +1,6 @@
+# List 
+# Lists are similar to vectors except they can store data of different types i.e., lists are heterogeneous data structures. We use the list() function to create a list.
+# combining more than one data type of your choice.
+cchub_list <- list(254, "Ilori Muideen", "Ikorodu", "Nigeria", "Kigali", "Camaroon")
+
+cchub_list
