f03662e08fe467136dca6c46e1ccbec0b0a24c3c
Muideen27 <ilorimuideen0000@gmail.com>
2024-05-17 12:40:05 +0100

    Row, Column and Dimentions

--- R_data_structure/4_array.R
+++ R_data_structure/4_array.R
@@ -7,3 +7,14 @@ arr1 <- array(c(1:18), dim = c(2, 3, 3))
 arr1
 
 # The dimension(2, 3, 3) simply means 2 rows, 3 column and 3 matrix
+
+arr <- array(c(1:48), dim = c(4,3,4))
+arr
+
+rownames <- c("row1", "row2", "row3", "row4")
+colnames <- c("col1", "col2", "col3", "row4")
+matrix_name <- c("Olu", "Gift", "Ajala")
+arr <- array(c(1:40), dim = c(4, 4, 3), dimnames = list(rownames, colnames))
+arr
+fac <-factor(arr)
+fac
