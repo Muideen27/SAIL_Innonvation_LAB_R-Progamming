b518b0b1895862dff98354a9d03f3e2f9f51ef8d
Muideen27 <ilorimuideen0000@gmail.com>
2024-05-17 12:00:26 +0100

    data structure manipulation

---
+++ R_data_structure/4_array.R
@@ -0,0 +1,7 @@
+# Array
+# Arrays are collections of matrices stacked on top of the other (homogeneous data structures). They are three dimensional.
+# The array() can be used to create an array.
+# array_name = array(data, dim, dimnames)
+
+arr1 <- array(c(1:18), dim = c(2, 3, 3))
+arr1
