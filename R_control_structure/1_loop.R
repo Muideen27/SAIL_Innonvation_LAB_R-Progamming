99cc76c00bb2da4cd64a6b80ee1d0a11382a0433
Muideen27 <ilorimuideen0000@gmail.com>
2024-05-18 11:34:00 +0100

    loop condition

---
+++ R_control_structure/1_loop.R
@@ -0,0 +1,15 @@
+# Decision Making & Loops (Control Structures)
+
+sch <- "Pretoria"
+
+if (sch == "Pretoria" | sch == "Windhoek" | sch == "Lusaka" | sch == "Harare") {
+  
+  print("Candidates falls within Southern geographical zone!")
+  
+} else {
+  
+  print("I am unsure if the candidate falls within area of interest")
+  
+}
+
+# The | sign is "OR" Operation
