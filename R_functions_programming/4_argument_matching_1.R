c6c45b59471bed8d527cc9ab89ab9ec0853fdb47
Muideen27 <ilorimuideen0000@gmail.com>
2024-05-31 16:52:32 +0100

    Update

--- R_functions_programming/4_argument_matching_1.R
+++ R_functions_programming/4_argument_matching_1.R
@@ -7,3 +7,35 @@ function (formula, data, subset, weights, na.action,
   
 lm(data = yourdata, y ~ x, model = FALSE, 1:100)
 lm(y ~ x, yourdata, 1:100, model = FALSE)
+
+your_function_name <- function(abuja, b = 1, c = 2, d = NULL) {
+  
+}
+
+#######
+# Lazy Evaluation_1
+
+f <- function(a, b) {
+  a^2
+} 
+f(2)
+
+#######
+# Lazy Evaluation_2
+ 
+f <- function(a, b) {
+  print(a)
+  print(b)
+} 
+
+f(234)
+
+###### 
+# Lazy Evaluation_3
+
+f <- function(a, b) {
+  print(a^3)
+  print(b^3)
+}
+
+f(234)
