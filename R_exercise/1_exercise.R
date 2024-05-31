4bace2a5f957b56130d875a4073b393f485d2305
Muideen27 <ilorimuideen0000@gmail.com>
2024-05-31 14:58:27 +0100

    updated

--- R_exercise/1_exercise.R
+++ R_exercise/1_exercise.R
@@ -1,3 +1,7 @@
+# Write a R program that will generate a random variable of 
+# binomial distribution and arrange it sequentially and print 
+# the arrangement
+
 # Custom bubble sort function
 bubble_sort <- function(x) {
   n <- length(x)
@@ -18,7 +22,7 @@ set.seed(123)
 
 # Parameter for binomial distribution
 n <- 10 # number of trials
-size <- 10 # number of experiments
+size <- 10 # number of experiments or the range of the number not more than 10
 prob <- 0.5 # probability of success in each trials
 
 
@@ -30,3 +34,21 @@ print(random_binomial)
 sorted_binomial <- bubble_sort(random_binomial)
 
 print(sorted_binomial)
+
+######
+# Method 2
+
+sequence <- function(num) {
+  sequ <- c()
+  while (lenght(num) > 0) {
+    leastt <- min(num)
+    sequ <- c(sequ, leastt)
+    num <- num[num != leastt | duplicated(num)]
+  }
+  return (sequ)
+}
+
+numbers <- rbinom( n = 10, size = 10, prob = 0.5)
+numbers
+numbers(sequence)
+
