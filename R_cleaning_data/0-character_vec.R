4104ebad50a1d0131b1321de662b68d7ffc4eebe
Muideen27 <ilorimuideen0000@gmail.com>
2024-06-20 11:50:34 +0100

    character data

---
+++ R_cleaning_data/0-character_vec.R
@@ -0,0 +1,11 @@
+# Character vectors - tolower(), toupper()
+
+ambulanceData <- read.csv("C:/Users/Open User/Desktop/Muideen_SAIL/nigeria_ambulances.csv")
+
+head(ambulanceData)
+
+# function to get or set the names of an Object
+names(ambulanceData)
+
+# Character Transission and case folding
+tolower(names(ambulanceData))
