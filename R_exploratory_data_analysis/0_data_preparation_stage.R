036fed23b894f03b9be6790a10260ef3a153f8d0
Muideen27 <ilorimuideen0000@gmail.com>
2024-06-20 15:59:05 +0100

    upated

---
+++ R_exploratory_data_analysis/0_data_preparation_stage.R
@@ -0,0 +1,17 @@
+# Data preparation_stage
+## read in our data in any format ... read.csv(), read_csv(), read_excel(), fromJSON()
+
+covid_data <- read.csv("C:/Users/Open User/Desktop/Muideen_SAIL/covid19_nigeria_states.csv")
+
+head(covid_data)
+names(covid_data)
+glimpse(covid_data)
+## Inspect the data ... head(), names(), glimpse()
+str(covid_data)
+
+## Select and group the data (if necessary) ... filter(), subset()
+covid_data %>% 
+  subset(states == "Kaduna") %>% 
+  group_by(states, month) %>% 
+  summarize(count = n()) %>% 
+  as.data.frame()
