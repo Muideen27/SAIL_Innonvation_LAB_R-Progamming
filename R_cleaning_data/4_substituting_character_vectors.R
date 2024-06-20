73035229d1acc18e7ae9ec10099258d114e21f77
Muideen27 <ilorimuideen0000@gmail.com>
2024-06-20 12:21:24 +0100

    substituting character vectors

---
+++ R_cleaning_data/4_substituting_character_vectors.R
@@ -0,0 +1,13 @@
+# Substituting character vectors
+
+# Read a CSV file containing ambulance data from Nigeria into a data frame
+ambulances <- read.csv("C:/Users/Open User/Desktop/Muideen_SAIL/nigeria_ambulances.csv")
+
+# Display the first 4 rows of the data frame to get an overview of the data
+head(ambulances, 4)
+
+# Retrieve and print the column names of the data frame
+names(ambulances)
+
+# Remove underscores from the column names and print the modified names
+sub("_", "", names(ambulances))
