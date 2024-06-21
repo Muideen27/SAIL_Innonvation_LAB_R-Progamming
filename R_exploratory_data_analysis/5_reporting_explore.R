679409ea7a23eb816c8c2d196a36d9675dff6def
Muideen27 <ilorimuideen0000@gmail.com>
2024-06-21 10:33:30 +0100

    reporting explore

---
+++ R_exploratory_data_analysis/5_reporting_explore.R
@@ -0,0 +1,10 @@
+# Reporting explore
+# Faster html plotting !
+  
+  ## Running this on the learning platform may take some minutes
+  # covid_data %>%
+  #   report(
+  #     target      = total_death,
+  #     output_dir  = "reports/",
+  #     output_file = "covid_death_nigerian_states_plots.html"
+  #   )
