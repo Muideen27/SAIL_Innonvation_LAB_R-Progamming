60d83cd9ce53134486283ad766a6a0e9ece419f8
Muideen27 <ilorimuideen0000@gmail.com>
2024-06-29 14:02:50 +0100

    obtaining resutls

---
+++ R_regression_analysis_2/1_obtaining_results.R
@@ -0,0 +1,14 @@
+# Load the necessary library
+library(datasets)
+
+# Fit a linear model with mpg as the response variable, and wt and cyl as the predictor variables
+model <- lm(mpg ~ wt + cyl, data = mtcars)
+
+# Display the summary of the fitted model
+summary(model)
+
+# If we want to fit the model without an intercept, we can use -1 in the formula
+model_no_intercept <- lm(mpg ~ wt + cyl - 1, data = mtcars)
+
+# Display the summary of the model without the intercept
+summary(model_no_intercept)
